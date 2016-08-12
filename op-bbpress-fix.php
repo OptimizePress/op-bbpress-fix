9f04d46adf665d02db3712ea4a09e30346fa92b5
Davor Jambor <davor.jambor@gmail.com>
2016-08-12 14:54:35 +0200

    Support for editing replies in bbPress forum by adding custom template return filter

--- op-bbpress-fix.php
+++ op-bbpress-fix.php
@@ -9,14 +9,14 @@
  * that starts the plugin.
  *
  * @link              http://www.optimizepress.com/
- * @since             1.0.0
+ * @since             1.0.1
  * @package           Op_bbPress_Fix
  *
  * @wordpress-plugin
- * Plugin Name:       OptimizePress bbPress plugin fix
+ * Plugin Name:       OptimizePress bbPress fix
  * Plugin URI:        http://www.optimizepress.com/
  * Description:       Allow rendering of forums on OptimizePress blog
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            OptimizePress
  * Author URI:        http://www.optimizepress.com/
  * License:           GPL-2.0+
@@ -27,15 +27,25 @@
 
 // If this file is called directly, abort.
 if ( ! defined( 'WPINC' ) ) {
-	die;
+    die;
 }
 
-function op_allow_bbforum() {
-    $forumPage = get_post(url_to_postid( "http://".$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'] ));
+if (!function_exists('op_allow_bbforum')) {
+    function op_allow_bbforum()
+    {
+        $forumPage = get_post(url_to_postid("http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']));
 
-    if ($forumPage->post_type == 'forum'){
-        op_update_option('blog_enabled','Y');
-        op_update_option('installed','Y');
+        if ($forumPage->post_type == 'forum') {
+            op_update_option('blog_enabled', 'Y');
+            op_update_option('installed', 'Y');
+        }
     }
 }
 add_action('init', 'op_allow_bbforum');
+
+if (!function_exists('bb_reply_renderPageTemplate')){
+    function bb_reply_renderPageTemplate(){
+        return OP_THEME_DIR . 'page.php';
+    }
+}
+add_filter('opRenderOtherTemplate','bb_reply_renderPageTemplate');
